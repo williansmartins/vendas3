e14d5e26e474ba440f47e1a668db71436a4b4d10
Willians Martins de Morais <willians.martins@digital.box>
2014-04-21 23:22:24 -0300

    add camada de teste e refatorando o build WEB

---
+++ projeto/src/br/com/exemplo/vendas/apresentacao/web/mb/MBTest.java
@@ -0,0 +1,124 @@
+package br.com.exemplo.vendas.apresentacao.web.mb;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.com.exemplo.vendas.apresentacao.service.Service;
+import br.com.exemplo.vendas.negocio.model.vo.UsuarioVO;
+import br.com.exemplo.vendas.util.Redirecionador;
+import br.com.exemplo.vendas.util.exception.LayerException;
+
+@SuppressWarnings("serial")
+@ManagedBean(name = "teste_mb")
+@SessionScoped
+public class MBTest implements Serializable
+{
+
+    Service service;
+    Boolean sucesso;
+    UsuarioVO vo;
+
+    public MBTest()
+    {
+	vo = gerarVO();
+	service = new Service();
+    }
+
+    public void redirecionar( )
+    {
+	new Redirecionador().redirecionar( "pagina-nova.xhtml" );
+    }
+
+    public void inserir( )
+    {
+	System.out.println("Inserindo...");
+	try
+	{
+	    sucesso = service.inserirUsuario( vo );
+	} catch ( LayerException e )
+	{
+	    System.out.println( "Exceção ao inserir: " + e.getMessage() );
+	}
+
+	if ( sucesso )
+	{
+	    System.out.println( "Sucesso ao inserir" );
+	} else
+	{
+	    System.out.println( "Erro ao inserir" );
+	}
+    }
+
+    public void remover( )
+    {
+	System.out.println("Removendo...");
+	service = new Service();
+	try
+	{
+	    service.excluirUsuario( vo );
+	}catch ( LayerException e )
+	{
+	    System.out.println( "Exceção ao inserir: " + e.getMessage() );
+	}
+    }
+    
+    public void atualizar( )
+    {
+	System.out.println("Atualizando...");
+	vo.setSenha( "nova-senha" );
+	service = new Service();
+	try
+	{
+	    service.alterarUsuario( vo );
+	}catch ( LayerException e )
+	{
+	    System.out.println( "Exceção ao inserir: " + e.getMessage() );
+	}
+    }
+    
+    public void listar( ) throws LayerException
+    {
+	System.out.println("Listando...");
+	service = new Service();
+	List<UsuarioVO> lista = service.listarUsuarios();
+	System.out.println( lista );
+    }
+    
+    private UsuarioVO gerarVO( )
+    {
+	String login = "teste-" + new Date().getTime();
+	String senha = ( "senha2" );
+	String grupo = ( "grupo2" );
+	String perfil = ( "perfil2" );
+
+	UsuarioVO vo = new UsuarioVO();
+	vo.setLogin( login );
+	vo.setSenha( senha );
+	vo.setGrupo( grupo );
+	vo.setPerfil( perfil );
+	vo.setBloqueado( true );	
+	vo.setUltimoAcesso( new Date() );
+	return vo;
+    }
+
+
+    // GETTERS AND SETTERS
+
+    public UsuarioVO getVo( )
+    {
+        return vo;
+    }
+
+    public void setVo( UsuarioVO vo )
+    {
+        this.vo = vo;
+    }
+
+    
+
+}
--- projeto/src/br/com/exemplo/vendas/apresentacao/web/mb/UsuarioMB.java
+++ projeto/src/br/com/exemplo/vendas/apresentacao/web/mb/UsuarioMB.java
@@ -2,13 +2,16 @@ package br.com.exemplo.vendas.apresentacao.web.mb;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import br.com.exemplo.vendas.apresentacao.service.Service;
 import br.com.exemplo.vendas.negocio.model.vo.UsuarioVO;
 import br.com.exemplo.vendas.util.Redirecionador;
+import br.com.exemplo.vendas.util.exception.LayerException;
 
 @SuppressWarnings("serial")
 @ManagedBean(name = "usuario_mb")
@@ -29,47 +32,89 @@ public class UsuarioMB implements Serializable
     {
 	new Redirecionador().redirecionar( "inserir-usuario.xhtml" );
     }
-    
-    public void redirecioarListagem( )
+
+    public void redirecinoarListagem( )
     {
 	new Redirecionador().redirecionar( "lista-usuarios.xhtml" );
     }
 
+    public void salvar( )
+    {
+	inserir();
+    }
+
     public void inserir( )
     {
-    }
-    
-    public void editar( )
-    {
-    }
-    
-    public void remover( )
-    {
-    }
-    
-    public void atualizar( )
-    {
-    }
-    
+	System.out.println( "Inserindo..." );
+	String login = vo.getLogin();
+	String senha = ( "senha2" );
+	String grupo = ( "grupo2" );
+	String perfil = ( "perfil2" );
+	String bloqueado = ( "bloqueado" );
+	String nome = ( "nome" );
 
+	// UsuarioVO vo = new UsuarioVO( login, senha, grupo, perfil, true, new
+	// Date() );
+	UsuarioVO vo = new UsuarioVO();
+	vo.setLogin( login );
+	vo.setSenha( senha );
+	vo.setGrupo( grupo );
+	vo.setPerfil( perfil );
+	vo.setUltimoAcesso( new Date() );
+
+	Service service = new Service();
+	Boolean sucesso = false;
+	try
+	{
+	    sucesso = service.inserirUsuario( vo );
+	} catch ( LayerException e )
+	{
+	    System.out.println( "Exceção ao inserir: " + e.getMessage() );
+	}
+
+	if ( sucesso )
+	{
+	    System.out.println( "Sucesso ao inserir" );
+	} else
+	{
+	    System.out.println( "Erro ao inserir" );
+	}
+    }
+
+    public void remover( )
+    {}
+
+    public void atualizar( )
+    {}
+
+    
+    public void teste( ) throws LayerException
+    {
+	Service service = new Service( ) ;
+	List<UsuarioVO> lista = service.listarUsuarios( ) ;
+	System.out.println(lista);
+    }
+    
     // GETTERS AND SETTERS
     public UsuarioVO getVo( )
     {
-	return vo;
+        return vo;
     }
 
     public void setVo( UsuarioVO vo )
     {
-	this.vo = vo;
+        this.vo = vo;
     }
 
     public List<UsuarioVO> getLista( )
     {
-	return lista;
+        return lista;
     }
 
     public void setLista( List<UsuarioVO> lista )
     {
-	this.lista = lista;
+        this.lista = lista;
     }
+
+ 
 }
