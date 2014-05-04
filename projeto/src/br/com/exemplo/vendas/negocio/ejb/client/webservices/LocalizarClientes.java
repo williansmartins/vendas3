7ca41af6bc0e1d10d2849027d9baa7afe87fa0db
Alberto Cerqueira <Alberto Cerqueira@AC-PC>
2014-05-04 10:32:03 -0300

    Ajustes classes webserivces.

--- projeto/src/br/com/exemplo/vendas/negocio/ejb/client/webservices/LocalizarClientes.java
+++ projeto/src/br/com/exemplo/vendas/negocio/ejb/client/webservices/LocalizarClientes.java
@@ -1,42 +1,21 @@
 package br.com.exemplo.vendas.negocio.ejb.client.webservices;
 
 import java.rmi.RemoteException;
-import java.util.Hashtable;
 
 import javax.jws.WebMethod;
 import javax.jws.WebService;
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-import br.com.exemplo.vendas.negocio.interfaces.ClienteInterface;
+import br.com.exemplo.vendas.apresentacao.service.Service;
 import br.com.exemplo.vendas.negocio.model.vo.ClienteVO;
-import br.com.exemplo.vendas.util.dto.ServiceDTO;
 import br.com.exemplo.vendas.util.exception.LayerException;
 
 @WebService
 public class LocalizarClientes {
 
 	@WebMethod
-	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public ClienteVO[] localizarClientesComComprasRealizadas() throws NamingException, RemoteException, LayerException {
-		Hashtable prop = new Hashtable();
-		prop.put(InitialContext.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
-		prop.put(InitialContext.PROVIDER_URL, "jnp://localhost:1099");
-		Context ctx = new InitialContext(prop);
-		ClienteInterface remoteCliente = (ClienteInterface) ctx.lookup("ClienteBean/remote");
-		ServiceDTO requestDTO = new ServiceDTO();
-		ServiceDTO responseDTO = new ServiceDTO();
-
-		requestDTO.set("loginCliente", "alberto");
-		responseDTO = remoteCliente.localizarClientesPorCompra(requestDTO);
-		ClienteVO[] clienteVOs = (ClienteVO[]) responseDTO.get("clientesPorCompraRealizadas");
-		if(clienteVOs != null){
-			for(int i = 0; i < clienteVOs.length; i++){
-				ClienteVO clienteVO = (ClienteVO) clienteVOs[i];
-				System.out.println(clienteVO);
-			}
-		}
-		return clienteVOs;
+		Service service = new Service();
+		return service.localizarClientesComComprasRealizadas();
 	}
 }
--- projeto/src/br/com/exemplo/vendas/negocio/ejb/client/webservices/LocalizarProdutos.java
+++ projeto/src/br/com/exemplo/vendas/negocio/ejb/client/webservices/LocalizarProdutos.java
@@ -14,27 +14,7 @@ import br.com.exemplo.vendas.util.exception.LayerException;
 public class LocalizarProdutos {
 
 	@WebMethod
-	//@SuppressWarnings({ "unchecked", "rawtypes" })
 	public ProdutoVO[] localizarProdutosPorQuantidadeAcimaDe2UnidadesEPrecoAbaixoDe1000() throws NamingException, RemoteException, LayerException {
-		/*
-		Hashtable prop = new Hashtable();
-		prop.put(InitialContext.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
-		prop.put(InitialContext.PROVIDER_URL, "jnp://localhost:1099");
-		Context ctx = new InitialContext(prop);
-		ProdutoInterface remoteProduto = (ProdutoInterface) ctx.lookup("ProdutoBean/remote");
-		ServiceDTO requestDTO = new ServiceDTO();
-		ServiceDTO responseDTO = new ServiceDTO();
-		requestDTO.set("preco", new BigDecimal(1000));
-		requestDTO.set("quantidadeEstoque", 2);
-		responseDTO = remoteProduto.localizarProdutosPorQuantidadeAcimaDeEPrecoAbaixoDe(requestDTO);
-		ProdutoVO[] produtoVOs = (ProdutoVO[]) responseDTO.get("produtosPorQuantidadeAcimaDeEPrecoAbaixoDe");
-		if(produtoVOs != null){
-			for(int i = 0; i < produtoVOs.length; i++){
-				ProdutoVO produtoVO = (ProdutoVO) produtoVOs[i];
-				System.out.println(produtoVO);
-			}
-		}
-		*/
 		Service service = new Service();
 		return service.localizarProdutosPorQuantidadeAcimaDe2UnidadesEPrecoAbaixoDe1000();
 	}
