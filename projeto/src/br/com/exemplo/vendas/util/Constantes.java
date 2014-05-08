7aaa75894384c6cf019a69ca33a5684357d5ff15
Alex <alexcaiom@gmail.com>
2014-05-08 00:58:05 -0300

    Classe Constantes

---
+++ projeto/src/br/com/exemplo/vendas/util/Constantes.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.com.exemplo.vendas.util;
+
+/**
+ * @author Alex
+ *
+ */
+public class Constantes {
+
+	public static final String MENSAGEM_FALHA_OPERACAO = "Desculpe, não foi possível processar sua solicitação. Tente novamente!";
+
+	
+	
+}
